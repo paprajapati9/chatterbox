5df8e89eafbbb4125dcbd412e79363756850bf80
paprajapati9 <paprajapati9@gmail.com>
2021-08-15 23:06:24 +0530

    modified dashboard

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,15 +1,42 @@
-import React from "react";
-import Dropdown from "../utilities/Dropdown";
+import React, {useState} from "react";
+import {auth} from "../../firebase";
 
 import "./user.css";
 
 export default function User(props) {
-	return (
-		<div className={props.hasDropdown ? 'user-dropdown user-container' : 'user-container'}>
+
+	const [isActive, setActive] = useState('');
+
+	const DropdownAction = (e) => {
+		e.preventDefault();
+		console.log(isActive, "isactive");
+		if(isActive) setActive(false)
+		else setActive(true);
+	}
+
+	window.onclick = function(e) {
+		var myDropdown = document.getElementById("dropdown-container");
+		if(!myDropdown) return;
+		if (!(e.target == myDropdown || myDropdown.contains(e.target)) && isActive) {
+			setActive(false)
+		}
+	}
+
+	let dropClass = 'user-dropdown user-container ';
+ 	return (
+		<div 
+			id = {props.hasDropdown && 'dropdown-container'}
+			className={props.hasDropdown ?  dropClass + (isActive && 'show '): 'user-container'} 
+			onClick = {props.hasDropdown && DropdownAction}
+		>
 			<img src={props.img || 'https://res.cloudinary.com/dqcsk8rsc/image/upload/v1577268053/avatar-1-bitmoji_upgwhc.png'} className="user-avatar"></img>
             <div className="user-name">{props.userName}</div>
-			{props.hasDropdown && <i class="fa fa-caret-down"></i>}
-			{props.hasDropdown && <Dropdown />}
+			{props.hasDropdown && <i className="fa fa-caret-down"></i>}
+			{props.hasDropdown && 
+			<div className="dropdown">
+				<div onClick = { () => {auth.signOut()} }>Sign Out</div>
+			</div>
+			}
 		</div>
 	)
 }
--- src/components/utilities/Dropdown.jsx
+++ src/components/utilities/Dropdown.jsx
@@ -2,10 +2,30 @@ import React from "react";
 
 import "./dropdown.css"
 
+export function DropdownAction(e) {
+	let dropcontainer = e.target.closest("#dropdown-container");
+	if(dropcontainer){
+		dropcontainer.classList.add("show");
+	}
+	console.log("here", dropcontainer);
+	
+	//.classList.add("show");
+}
+  
+//Close the dropdown if the user clicks outside of it
+// window.onclick = function(e) {
+// 	if (!e.target.matches('.dropbtn')) {
+// 	var myDropdown = document.getElementById("dropdown-container");
+// 		if (myDropdown.classList.contains('show')) {
+// 			myDropdown.classList.remove('show');
+// 		}
+// 	}
+// }
+
 export default function Dropdown(props) {
 	return (
 		<div className="dropdown">
-            <a>Sign Out</a>
+            <div >Sign Out</div>
         </div>
 	)
 }
